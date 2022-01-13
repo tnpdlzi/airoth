4031444b43af586bb2639c29e9350777267e3211
Lee Donggun <tnpdlzi@naver.com>
2022-01-13 15:33:41 +0900

    init commit

---
+++ src/main/java/com/donggun/airoth/AirothApplication.java
@@ -0,0 +1,13 @@
+package com.donggun.airoth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirothApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donggun/airoth/AirothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donggun.airoth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
